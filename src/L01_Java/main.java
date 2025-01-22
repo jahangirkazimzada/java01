d9589de5f588cf4916399cb139702659cf485c3c
Jahangir Kazimzada <ckazimzada02@gmail.com>
2025-01-22 22:39:54 +0400

    java

---
+++ src/L01_Java/main.java
@@ -0,0 +1,9 @@
+package L01_Java;
+
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("fist code");
+    }
+}
