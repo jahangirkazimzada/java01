6b3c9f3908ed46fdb411e11a145a8a645e62d1f8
Jahangir Kazimzada <ckazimzada02@gmail.com>
2025-01-25 19:36:30 +0400

    new

--- src/L01_Java/main.java
+++ src/L01_Java/main.java
@@ -17,17 +17,29 @@ public class main {
 
         //TO GET USER INFO
 
-        Scanner scan  = new Scanner(System.in);
-        System.out.println("Name: ");
-        String name = scan.next();
-        System.out.println("Surname: ");
-        String surname = scan.next();
-        System.out.println("Age: ");
-        int age = scan.nextInt();
-        System.out.println(
-                "Name: " + name +
-                "\nSurname: " + surname +
-                "\nAge: " + age
-        );
+//        Scanner scan  = new Scanner(System.in);
+//        System.out.println("Name: ");
+//        String name = scan.next();
+//        System.out.println("Surname: ");
+//        String surname = scan.next();
+//        System.out.println("Age: ");
+//        int age = scan.nextInt();
+//        System.out.println(
+//                "Name: " + name +
+//                "\nSurname: " + surname +
+//                "\nAge: " + age
+//        );
+
+        Scanner scan = new Scanner(System.in);
+        int n1 = scan.nextInt();//25
+        int n2 = scan.nextInt();//40
+        int temp = 0;//n1 = 25, n2 = 40, temp = 0
+        temp = n2;//n1 = 25, n2 = 40, temp = 40
+        n2 = n1;//n1 = 25, n2 = 25, temp = 40
+        n1 = temp;
+
+        System.out.println("Result: " + n1 + ", " + n2);
+
+
     }
 }
